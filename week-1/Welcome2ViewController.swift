fb794597495a95bcd29e9eafd833d9134c01b6df
golivia <olivia@igg-a92.lan>
2014-09-14 16:30:48 -0700

    Welcome and split to create/signIn
    Welcome and split to create/signIn

---
+++ week-1/Welcome2ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Welcome2ViewController.swift
+//  week-1
+//
+//  Created by Olivia Muesse on 9/10/14.
+//  Copyright (c) 2014 Olivia Muesse. All rights reserved.
+//
+
+import UIKit
+
+class Welcome2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func clickPreviousButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
