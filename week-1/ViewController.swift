e2fc3c9e1f80587e6614bf9471795b6a00637be3
golivia <olivia@IGG-A92.local>
2014-09-14 23:31:43 -0700

    sign in tab bar

--- week-1/ViewController.swift
+++ week-1/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickBackNav(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
 
 }
 
