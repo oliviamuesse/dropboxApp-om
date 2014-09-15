7f3f87126c289408e1e9e0248a4e53b77f2a6cab
golivia <olivia@IGG-A92.local>
2014-09-14 22:57:06 -0700

    tab bar and scroll view
    tab bar and scroll view

---
+++ week-1/ScrollViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ScrollViewController.swift
+//  week-1
+//
+//  Created by Olivia Muesse on 9/14/14.
+//  Copyright (c) 2014 Olivia Muesse. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 772)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
