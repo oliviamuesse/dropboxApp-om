c34a3497804f9d8dac6bf0e0f275a0cd5c85387b
golivia <olivia@igg-a92.lan>
2014-09-16 19:10:25 -0700

    Very annoying persistent bug with tab bar controller fixed

--- week-1/ScrollViewController.swift
+++ week-1/ScrollViewController.swift
@@ -18,6 +18,8 @@ class ScrollViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
